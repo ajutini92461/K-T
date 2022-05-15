38d1f0f49bcac3c75d85a1d0d154dd91a781d33e
ajutini92461 <ajutini92461@universidadean.edu.co>
2022-05-15 00:40:40 -0500

    commit

---
+++ popup.js
@@ -0,0 +1,22 @@
+var preview = document.querySelector(".popup-productos");
+var previewBox = preview.querySelectorAll(".preview");
+
+document.querySelectorAll(".productos-container .boton-productos").forEach(boton => {
+    boton.onclick = () => {
+        preview.style.display = "flex";
+        var nombre = boton.getAttribute("data-name");
+        previewBox.forEach(preview => {
+            var target = preview.getAttribute("data-target");
+            if (nombre == target) {
+                preview.classList.add("activo");
+            }
+        });
+    };
+});
+
+previewBox.forEach(close => {
+    close.querySelector(".btn-rojo").onclick = () => {
+        close.classList.remove("activo");
+        preview.style.display = "none";
+    };
+});
